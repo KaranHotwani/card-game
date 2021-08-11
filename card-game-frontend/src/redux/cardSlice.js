5a9c503751b9749d04036cb8b5e8ea1a7e99908e
Karan H <karanhotwani.kh@gmail.com>
2021-08-11 20:28:08 +0530

    Initial redux setup wip

---
+++ card-game-frontend/src/redux/cardSlice.js
@@ -0,0 +1,67 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cardSlice = createSlice({
+    name:"cards",
+    initialState:{
+        cardValues:[
+            {
+                type:"Cat",
+                isOpen:false
+            },
+            {
+                type:"Shuffle",
+                isOpen:false
+            },
+            {
+                type:"Cat",
+                isOpen:false
+            },
+            {
+                type:"ExplodingK",
+                isOpen:false
+            },
+            {
+                type:"Defuse",
+                isOpen:false
+            }
+        ],
+        availableCardValues:["Cat","Shuffle","ExplodingK","Defuse"],
+        user:"No User",
+        defuseCardCount:0,
+        gameStatus:"NS",//"NS,W,L"
+    },
+    reducers:{
+        showCard: (state,action)=>{
+            const type = action.payload.type;
+            for(let i=0;i<state.cardValues.length;i++)
+            {
+                const card = state.cardValues[i];
+                if(card.type===type && !card.isOpen)
+                {
+                    state.cardValues[i].isOpen = true;
+                }
+            }
+        },
+        removeCard:(state,action)=>{
+
+        },
+        generateRandomCards:(state,action)=>{
+
+        },
+        incrementDefuseCount:(state,action)=>{
+
+        },
+        decrementDefuseCount:(state,action)=>{
+
+        },
+        setUser:(state,action)=>{
+
+        },
+        setGameStatus:(state,action)=>{
+
+        },
+    }
+})
+
+export const { showCard, removeCard, generateRandomCards , incrementDefuseCount , decrementDefuseCount, setUser, setGameStatus} = cardSlice.actions;
+export default cardSlice.reducer;
